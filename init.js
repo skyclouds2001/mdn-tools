889785f38dd8e488bc496b081a2e13bd0af3db06
skyclouds2001 <skyclouds2001@163.com>
2024-12-03 12:53:35 +0800

    feat: use source from repo main branch than npm package

--- init.js
+++ init.js
@@ -3,3 +3,7 @@ import child_process from 'node:child_process'
 child_process.execSync('npm ci')
 
 child_process.execSync('git clone https://github.com/mdn/content.git ./node_modules/@mdn/content')
+
+child_process.execSync('git clone https://github.com/mdn/browser-compat-data.git ./node_modules/@mdn/browser-compat-data')
+
+child_process.execSync('git clone https://github.com/mdn/data.git ./node_modules/mdn-data')
