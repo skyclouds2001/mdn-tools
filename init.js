5460abfc8baa5b4246baff14f4072326eb937379
skyclouds2001 <skyclouds2001@163.com>
2024-12-10 23:46:09 +0800

    feat: update init script

--- init.js
+++ init.js
@@ -1,9 +1,50 @@
 import child_process from 'node:child_process'
+import fs from 'node:fs'
+import path from 'node:path'
+import process from 'node:process'
 
-child_process.execSync('npm ci')
+const root = process.cwd()
 
-child_process.execSync('git clone https://github.com/mdn/content.git ./@mdn/content')
+child_process.exec('npm ci', {
+  cwd: root,
+}, (error) => {
+  if (error != null) {
+    console.log(error)
+  }
+})
 
-child_process.execSync('git clone https://github.com/mdn/browser-compat-data.git ./@mdn/browser-compat-data')
+if (fs.existsSync(path.resolve(root, '@mdn/browser-compat-data'))) {
+  child_process.exec('git pull', {
+    cwd: path.resolve(root, '@mdn/browser-compat-data'),
+  }, (error) => {
+    if (error != null) {
+      console.log(error)
+    }
+  })
+} else {
+  child_process.exec('git clone https://github.com/mdn/browser-compat-data.git ./@mdn/browser-compat-data', {
+    cwd: root,
+  }, (error) => {
+    if (error != null) {
+      console.log(error)
+    }
+  })
+}
 
-child_process.execSync('git clone https://github.com/mdn/data.git ./mdn-data')
+if (fs.existsSync(path.resolve(root, 'mdn-data'))) {
+  child_process.exec('git pull', {
+    cwd: path.resolve(root, 'mdn-data'),
+  }, (error) => {
+    if (error != null) {
+      console.log(error)
+    }
+  })
+} else {
+  child_process.exec('git clone https://github.com/mdn/data.git ./mdn-data', {
+    cwd: root,
+  }, (error) => {
+    if (error != null) {
+      console.log(error)
+    }
+  })
+}
