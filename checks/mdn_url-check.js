59de6a1b4bf804e6a367f320bc27d6e1529f566c
skyclouds2001 <skyclouds2001@163.com>
2024-12-19 14:50:50 +0800

    fix: resolve with basic selectors

--- checks/mdn_url-check.js
+++ checks/mdn_url-check.js
@@ -29,7 +29,7 @@ for (const at_rule in at_rule_data) {
     if (at_rule_data[at_rule]['mdn_url'] !== `https://developer.mozilla.org/docs/Web/CSS/${at_rule}`) {
       mismatch_mdn_url.add(at_rule)
     }
-    if (!fs.existsSync(path.resolve(root, '@mdn/content/files/en-us/web/css', at_rule.toLowerCase()))) {
+    if (!fs.existsSync(path.resolve(root, '@mdn/content/files/en-us/web/css', at_rule))) {
       missing_mdn_content.add(at_rule)
     }
   }
@@ -44,7 +44,7 @@ for (const at_rule in at_rule_data) {
         if (at_rule_data[at_rule]['descriptors'][at_rule_descriptor]['mdn_url'] !== `https://developer.mozilla.org/docs/Web/CSS/${at_rule}/${at_rule_descriptor}`) {
           mismatch_mdn_url.add(`${at_rule}/${at_rule_descriptor}`)
         }
-        if (!fs.existsSync(path.resolve(root, '@mdn/content/files/en-us/web/css', at_rule.toLowerCase(), at_rule_descriptor.toLowerCase()))) {
+        if (!fs.existsSync(path.resolve(root, '@mdn/content/files/en-us/web/css', at_rule, at_rule_descriptor))) {
           missing_mdn_content.add(`${at_rule}/${at_rule_descriptor}`)
         }
       }
@@ -59,7 +59,7 @@ for (const func in function_data) {
     if (function_data[func]['mdn_url'] !== `https://developer.mozilla.org/docs/Web/CSS/${func}`) {
       mismatch_mdn_url.add(func)
     }
-    if (!fs.existsSync(path.resolve(root, '@mdn/content/files/en-us/web/css', func.replaceAll('()', '').toLowerCase()))) {
+    if (!fs.existsSync(path.resolve(root, '@mdn/content/files/en-us/web/css', func.replaceAll('()', '')))) {
       missing_mdn_content.add(func)
     }
   }
@@ -72,7 +72,7 @@ for (const property in property_data) {
     if (property_data[property]['mdn_url'] !== `https://developer.mozilla.org/docs/Web/CSS/${property}`) {
       mismatch_mdn_url.add(property)
     }
-    if (!fs.existsSync(path.resolve(root, '@mdn/content/files/en-us/web/css', property.toLowerCase()))) {
+    if (!fs.existsSync(path.resolve(root, '@mdn/content/files/en-us/web/css', property))) {
       missing_mdn_content.add(property)
     }
   }
@@ -85,7 +85,7 @@ for (const selector in selector_data) {
     if (selector_data[selector]['mdn_url'] !== `https://developer.mozilla.org/docs/Web/CSS/${selector.replaceAll(' ', '_')}`) {
       mismatch_mdn_url.add(selector)
     }
-    if (!fs.existsSync(path.resolve(root, '@mdn/content/files/en-us/web/css', selector.replaceAll('::', '_doublecolon_').replaceAll(':', '_colon_').toLowerCase()))) {
+    if (!fs.existsSync(path.resolve(root, '@mdn/content/files/en-us/web/css', selector.replaceAll(' ', '_').replaceAll('::', '_doublecolon_').replaceAll(':', '_colon_')))) {
       missing_mdn_content.add(selector)
     }
   }
@@ -98,7 +98,7 @@ for (const type in type_data) {
     if (type_data[type]['mdn_url'] !== `https://developer.mozilla.org/docs/Web/CSS/${type}`) {
       mismatch_mdn_url.add(type)
     }
-    if (!fs.existsSync(path.resolve(root, '@mdn/content/files/en-us/web/css', type.toLowerCase()))) {
+    if (!fs.existsSync(path.resolve(root, '@mdn/content/files/en-us/web/css', type))) {
       missing_mdn_content.add(type)
     }
   }
