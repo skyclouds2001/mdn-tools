0c49d7f9f8f10b29ec3ff9ddd426475a37c126b3
skyclouds2001 <skyclouds2001@163.com>
2024-12-19 12:14:14 +0800

    feat: update init script

--- init.js
+++ init.js
@@ -2,78 +2,59 @@ import child_process from 'node:child_process'
 import fs from 'node:fs'
 import path from 'node:path'
 import process from 'node:process'
-import util from 'node:util'
 
-init()
+const root = process.cwd()
 
-async function init() {
-  const root = process.cwd()
+child_process.execSync('npm ci', {
+  cwd: root,
+})
 
-  const exec = util.promisify(child_process.exec)
+if (!fs.existsSync(path.resolve(root, '@mdn', '.gitignore'))) {
+  fs.writeFileSync(path.resolve(root, '@mdn', '.gitignore'), '*', 'utf-8')
+}
 
-  await exec('npm ci', {
-    cwd: root,
-  }, (error) => {
-    if (error != null) {
-      console.log(error)
-    }
+if (fs.existsSync(path.resolve(root, '@mdn/browser-compat-data'))) {
+  child_process.execSync('git pull', {
+    cwd: path.resolve(root, '@mdn/browser-compat-data'),
   })
 
-  if (!fs.existsSync(path.resolve(root, '@mdn', '.gitignore'))) {
-    fs.writeFileSync(path.resolve(root, '@mdn', '.gitignore'), '*', 'utf-8')
-  }
+  child_process.execSync('npm ci', {
+    cwd: path.resolve(root, '@mdn/browser-compat-data'),
+  })
 
-  if (fs.existsSync(path.resolve(root, '@mdn/browser-compat-data'))) {
-    await exec('git pull', {
-      cwd: path.resolve(root, '@mdn/browser-compat-data'),
-    }, (error) => {
-      if (error != null) {
-        console.log(error)
-      }
-    })
-  } else {
-    await exec('git clone https://github.com/mdn/browser-compat-data.git ./@mdn/browser-compat-data', {
-      cwd: root,
-    }, (error) => {
-      if (error != null) {
-        console.log(error)
-      }
-    })
-  }
+  child_process.execSync('npm run build', {
+    cwd: path.resolve(root, '@mdn/browser-compat-data'),
+  })
+} else {
+  child_process.execSync('git clone https://github.com/mdn/browser-compat-data.git ./@mdn/browser-compat-data', {
+    cwd: root,
+  })
+}
 
-  if (fs.existsSync(path.resolve(root, '@mdn/data'))) {
-    await exec('git pull', {
-      cwd: path.resolve(root, '@mdn/data'),
-    }, (error) => {
-      if (error != null) {
-        console.log(error)
-      }
-    })
-  } else {
-    await exec('git clone https://github.com/mdn/data.git ./@mdn/data', {
-      cwd: root,
-    }, (error) => {
-      if (error != null) {
-        console.log(error)
-      }
-    })
-  }
+if (fs.existsSync(path.resolve(root, '@mdn/data'))) {
+  child_process.execSync('git pull', {
+    cwd: path.resolve(root, '@mdn/data'),
+  })
 
-  if (fs.existsSync(path.resolve(root, '@mdn/content'))) {
-    await exec('git pull', {
-      cwd: path.resolve(root, '@mdn/content'),
-    }, (error) => {
-      if (error != null) {
-        console.log(error)
-      }
-    })
-  } else {
-    await exec('git clone https://github.com/mdn/content.git ./@mdn/content', {
-      cwd: root,
-    }, (error) => {
-      if (error != null) {
-        console.log(error)
-      }
-    })
-  }
+  child_process.execSync('npm ci', {
+    cwd: path.resolve(root, '@mdn/data'),
+  })
+} else {
+  child_process.execSync('git clone https://github.com/mdn/data.git ./@mdn/data', {
+    cwd: root,
+  })
+}
+
+if (fs.existsSync(path.resolve(root, '@mdn/content'))) {
+  child_process.execSync('git pull', {
+    cwd: path.resolve(root, '@mdn/content'),
+  })
+
+  child_process.execSync('yarn', {
+    cwd: path.resolve(root, '@mdn/content'),
+  })
+} else {
+  child_process.execSync('git clone https://github.com/mdn/content.git ./@mdn/content', {
+    cwd: root,
+  })
 }
