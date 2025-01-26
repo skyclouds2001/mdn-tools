04e40bbe0be164282f4f2569f4e8116a52f6bd6a
skyclouds2001 <skyclouds2001@163.com>
2025-01-26 23:33:53 +0800

    feat: rewrite init script

--- scripts/init.js
+++ scripts/init.js
@@ -9,52 +9,43 @@ child_process.execSync('npm ci', {
   cwd: root,
 })
 
-if (!fs.existsSync(path.resolve(root, '@mdn', '.gitignore'))) {
-  fs.writeFileSync(path.resolve(root, '@mdn', '.gitignore'), '*', 'utf-8')
-}
-
-if (fs.existsSync(path.resolve(root, '@mdn/browser-compat-data'))) {
-  child_process.execSync('git pull', {
-    cwd: path.resolve(root, '@mdn/browser-compat-data'),
-  })
-
-  child_process.execSync('npm ci', {
-    cwd: path.resolve(root, '@mdn/browser-compat-data'),
-  })
-
-  child_process.execSync('npm run build', {
-    cwd: path.resolve(root, '@mdn/browser-compat-data'),
-  })
-} else {
-  child_process.execSync('git clone https://github.com/mdn/browser-compat-data.git ./@mdn/browser-compat-data', {
+if (!fs.existsSync(path.resolve(root, '@mdn/data')) || !fs.existsSync(path.resolve(root, '@mdn/browser-compat-data')) || !fs.existsSync(path.resolve(root, '@mdn/content'))) {
+  child_process.execSync('git submodule init', {
     cwd: root,
   })
 }
 
-if (fs.existsSync(path.resolve(root, '@mdn/data'))) {
-  child_process.execSync('git pull', {
-    cwd: path.resolve(root, '@mdn/data'),
-  })
+child_process.execSync('git submodule sync --recursive', {
+  cwd: root,
+})
 
-  child_process.execSync('npm ci', {
-    cwd: path.resolve(root, '@mdn/data'),
-  })
-} else {
-  child_process.execSync('git clone https://github.com/mdn/data.git ./@mdn/data', {
-    cwd: root,
-  })
-}
+child_process.execSync('git submodule update --recursive --remote', {
+  cwd: root,
+})
 
-if (fs.existsSync(path.resolve(root, '@mdn/content'))) {
-  child_process.execSync('git pull', {
-    cwd: path.resolve(root, '@mdn/content'),
-  })
+child_process.execSync('git pull', {
+  cwd: path.resolve(root, '@mdn/data'),
+})
 
-  child_process.execSync('yarn', {
-    cwd: path.resolve(root, '@mdn/content'),
-  })
-} else {
-  child_process.execSync('git clone https://github.com/mdn/content.git ./@mdn/content', {
-    cwd: root,
-  })
-}
+child_process.execSync('npm ci', {
+  cwd: path.resolve(root, '@mdn/data'),
+})
+
+child_process.execSync('git pull', {
+  cwd: path.resolve(root, '@mdn/browser-compat-data'),
+})
+
+child_process.execSync('npm ci', {
+  cwd: path.resolve(root, '@mdn/browser-compat-data'),
+})
+
+child_process.execSync('npm run build', {
+  cwd: path.resolve(root, '@mdn/browser-compat-data'),
+})
+child_process.execSync('git pull', {
+  cwd: path.resolve(root, '@mdn/content'),
+})
+
+child_process.execSync('yarn', {
+  cwd: path.resolve(root, '@mdn/content'),
+})
